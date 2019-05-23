84e656b36d0b8d9b0569b8a4944149aee51279ec
s <s@jdlab.org>
2019-05-23 19:19:52 +0900

    update conf

--- conf.py
+++ conf.py
@@ -101,7 +101,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -142,7 +143,7 @@ html_context = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['manual']
 
-html_style = 'css/my_theme.css' 
+#html_style = 'css/my_theme.css'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
